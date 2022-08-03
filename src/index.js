86a2cef2fe1eda5606011a34e510f6eb508e5526
nikita filatov <110080690+HaCTIJle@users.noreply.github.com>
2022-08-03 10:14:57 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,129 @@
 module.exports = function toReadable (number) {
-  
+  const number = (num) =>{
+    if(num === 0){
+        return 'zero'
+    }else if(num === 1){
+        return 'one'
+    }else if(num === 2){
+        return 'two'
+    }else if(num === 3){
+        return 'three'
+    }else if(num === 4){
+        return 'four'
+    }else if(num === 5){
+        return 'five'
+    }else if(num === 6){
+        return 'six'
+    }else if(num === 7){
+        return 'seven'
+    }else if(num === 8){
+        return 'eight'
+    }else if(num === 9){
+        return 'nine'
+    }else if(num === 10){
+        return 'ten'
+    }else if(num === 11){
+        return 'eleven'
+    }else if(num === 12){
+        return 'twelve'
+    }else if(num === 13){
+        return 'thirteen'
+    }else if(num === 14){
+        return 'fourteen'
+    }else if(num === 15){
+        return 'fifteen'
+    }else if(num === 16){
+        return 'sixteen'
+    }else if(num === 17){
+        return 'seventeen'
+    }else if(num === 18){
+        return 'eighteen'
+    }else if(num === 19){
+        return 'nineteen'
+    }else if(num === 20){
+        return 'twenty'
+    }else if(num === 21){
+        return 'twenty one'
+    }else if(num === 22){
+        return 'twenty two'
+    }else if(num === 23){
+        return 'twenty three'
+    }else if(num === 24){
+        return 'twenty four'
+    }else if(num === 25){
+        return 'twenty five'
+    }else if(num === 26){
+        return 'twenty six'
+    }else if(num === 27){
+        return 'twenty seven'
+    }else if(num === 28){
+        return 'twenty eight'
+    }else if(num === 29){
+        return 'twenty nine'
+    }else if(num === 30){
+        return 'Two'
+    }else if(num === 31){
+        return 'Two'
+    }else if(num === 32){
+        return 'Two'
+    }else if(num === 33){
+        return 'Two'
+    }else if(num === 34){
+        return 'Two'
+    }else if(num === 35){
+        return 'Two'
+    }else if(num === 36){
+        return 'Two'
+    }else if(num === 37){
+        return 'Two'
+    }else if(num === 38){
+        return 'Two'
+    }else if(num === 39){
+        return 'Two'
+    }else if(num === 40){
+        return 'Two'
+    }else if(num === 41){
+        return 'Two'
+    }else if(num === 42){
+        return 'Two'
+    }else if(num === 43){
+        return 'Two'
+    }else if(num === 44){
+        return 'Two'
+    }else if(num === 45){
+        return 'Two'
+    }else if(num === 46){
+        return 'Two'
+    }else if(num === 47){
+        return 'Two'
+    }else if(num === 48){
+        return 'Two'
+    }else if(num === 49){
+        return 'Two'
+    }else if(num === 50){
+        return 'Two'
+    }else if(num === 51){
+        return 'Two'
+    }else if(num === 52){
+        return 'Two'
+    }else if(num === 53){
+        return 'Two'
+    }else if(num === 54){
+        return 'Two'
+    }else if(num === 55){
+        return 'Two'
+    }else if(num === 56){
+        return 'Two'
+    }else if(num === 57){
+        return 'Two'
+    }else if(num === 58){
+        return 'Two'
+    }else if(num === 59){
+        return 'Two'
+    }else if(num === 60){
+        return 'Two'
+    }
+};
+
+
 }
